bc813c08643e4adb4672a561d631432c08ef9c03
Rasoul <RasoulMotie1380@gmail.com>
2022-08-03 14:56:32 +0430

    work on profile fragment design and it's bottomSheet,almost completed

--- app/src/main/java/com/example/unsplashproject/fragments/profilefragment/ProfileFragment.kt
+++ app/src/main/java/com/example/unsplashproject/fragments/profilefragment/ProfileFragment.kt
@@ -5,9 +5,17 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.LinearLayout
 import com.example.unsplashproject.R
+import com.google.android.material.bottomsheet.BottomSheetDialog
 
 class ProfileFragment : Fragment() {
+
+    private lateinit var firstName:LinearLayout
+    private lateinit var lastName:LinearLayout
+    private lateinit var email:LinearLayout
+    private lateinit var password:LinearLayout
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -19,7 +27,43 @@ class ProfileFragment : Fragment() {
 
     private fun view(inflater: LayoutInflater, container: ViewGroup?): View {
         val view: View = inflater.inflate(R.layout.fragment_profile, container, false)
+        init(view)
         return view
+    }
+
+    private fun init(view: View) {
+        bindView(view)
+        firstName.setOnClickListener {
+            val viewBottomSheet:View=layoutInflater.inflate(R.layout.first_name_bottom_sheet_design,null)
+            val dialog=BottomSheetDialog(context!!)
+            dialog.setContentView(viewBottomSheet)
+            dialog.show()
+        }
+        lastName.setOnClickListener {
+            val viewBottomSheet:View=layoutInflater.inflate(R.layout.last_name_bottom_sheet_design,null)
+            val dialog=BottomSheetDialog(context!!)
+            dialog.setContentView(viewBottomSheet)
+            dialog.show()
+        }
+        email.setOnClickListener {
+            val viewBottomSheet:View=layoutInflater.inflate(R.layout.email_bottom_sheet_design,null)
+            val dialog=BottomSheetDialog(context!!)
+            dialog.setContentView(viewBottomSheet)
+            dialog.show()
+        }
+        password.setOnClickListener {
+            val viewBottomSheet:View=layoutInflater.inflate(R.layout.password_bottom_sheet_design,null)
+            val dialog=BottomSheetDialog(context!!)
+            dialog.setContentView(viewBottomSheet)
+            dialog.show()
+        }
+    }
+
+    private fun bindView(view: View) {
+        firstName=view.findViewById(R.id.ic_first_name)
+        lastName=view.findViewById(R.id.ic_last_name)
+        email=view.findViewById(R.id.ic_email)
+        password=view.findViewById(R.id.ic_password)
     }
 
 }
